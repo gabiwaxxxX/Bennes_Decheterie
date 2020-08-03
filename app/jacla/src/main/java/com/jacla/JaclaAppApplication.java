9aeb52efbd4b9f482d51354063094d566b6e2cf5
theolemague <theo.lm@hotmail.fr>
2020-08-03 14:31:41 +0200

    maven project

---
+++ app/jacla/src/main/java/com/jacla/JaclaAppApplication.java
@@ -0,0 +1,27 @@
+package com.jacla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.swing.JFrame;
+
+@SpringBootApplication
+public class JaclaAppApplication extends JFrame {
+	
+	public JaclaAppApplication() {
+        initUI();
+    }
+
+    private void initUI() {       
+        setTitle("Simple example");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+	public static void main(String[] args) {
+		var app = new JaclaAppApplication();
+		app.setVisible(true);
+		SpringApplication.run(JaclaAppApplication.class, args);
+	}
+
+}
