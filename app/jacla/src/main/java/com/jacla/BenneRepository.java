9d36a08af457d6f3e2baec34e7d46ed7cb1a7fb6
gabrielfournier1 <gabriel.fournier@live.fr>
2020-08-12 19:12:57 +0200

    working with persistance

--- app/jacla/src/main/java/com/jacla/BenneRepository.java
+++ app/jacla/src/main/java/com/jacla/BenneRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import org.springframework.data.repository.CrudRepository;
+
+
 @RepositoryRestResource(collectionResourceRel = "benne", path = "benne")
-public interface BenneRepository extends PagingAndSortingRepository<Benne, Long> {
+public interface BenneRepository extends CrudRepository<Benne, Long> {
 
   List<Benne> findByBenneName(@Param("name") String name);
 
